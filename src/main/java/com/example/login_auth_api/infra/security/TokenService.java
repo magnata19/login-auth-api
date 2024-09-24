abe2b6e6b2fa9015e4c6d8bcd9c49a87d0ffec0c
magnata19 <dehpacifico@gmail.com>
2024-09-23 23:41:38 -0300

    Initial commit

---
+++ src/main/java/com/example/login_auth_api/infra/security/TokenService.java
@@ -0,0 +1,38 @@
+package com.example.login_auth_api.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.example.login_auth_api.domain.user.User;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    private static final String SECRET_KEY = "a38ea1685bcaa9b816bf942227a71bec";
+
+    public String generateToken(User user) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(SECRET_KEY);
+            String token = JWT.create()
+                    .withIssuer("login-auth-api")
+                    .withSubject(user.getEmail())
+                    .withIssuedAt(generateExpirationDate())
+                    .sign(algorithm);
+            return token;
+        } catch (JWTCreationException ex)  {
+            throw new RuntimeException("Error while trying to create a token!");
+        }
+    }
+
+
+
+    private Instant generateExpirationDate() {
+        return LocalDateTime.now().plusMinutes(10).toInstant(ZoneOffset.of("3"));
+    }
+}
